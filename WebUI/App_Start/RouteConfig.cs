8f41d2da09ebff32b3b931b2ca4a1efe3a77a531
IvanSorokin <iwan954@mail.ru>
2016-04-30 02:34:13 +0600

    refresh

--- WebUI/App_Start/RouteConfig.cs
+++ WebUI/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "AdvancedSearch",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Character", action = "AdvancedList", id = "" }
+            );
+
+            routes.MapRoute(
                 name: "Search",
                 url: "{controller}/{action}/{fieldName}/{fieldValue}",
                 defaults: new { controller = "Character", action = "List", id = UrlParameter.Optional }
             );
+
+            
 
             routes.MapRoute(
                 name: "Default",
--- WebUI/Controllers/CharacterController.cs
+++ WebUI/Controllers/CharacterController.cs
@@ -30,10 +30,23 @@ namespace WebUI.Controllers
                 .Where(c => c.Name.Contains(name) && c.IsAlive == isAlive && c.Sex == sex).ToList();
         }
 
+
+        [HttpGet]
+        public ViewResult AdvancedList(string name, bool isAlive, Sex sex)
+        {
+            var view = View("List",new MainModel(FilterByParameters(name, isAlive, sex), Request.RawUrl));
+            view.ViewBag.Name = name;
+            view.ViewBag.IsAlive = isAlive;
+            view.ViewBag.Sex = sex;
+            return view;
+        }
+
         [HttpPost]
         public ViewResult List(string name, bool isAlive, Sex sex)
         {
-            var view = View(new MainModel(FilterByParameters(name, isAlive, sex), Request.RawUrl));
+
+            var backUrl = string.Format("/Character/AdvancedList?name={0}&isAlive={1}&sex={2}", name, isAlive, sex);
+            var view = View(new MainModel(FilterByParameters(name, isAlive, sex), backUrl));
             view.ViewBag.Name = name;
             view.ViewBag.IsAlive = isAlive;
             view.ViewBag.Sex = sex;
