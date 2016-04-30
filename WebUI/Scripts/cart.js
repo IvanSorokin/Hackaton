58a3b5ee69945dd13403cca418893cf3ba81e5c2
Mironov Alexey <mironov.alexey.fiit.urfu@gmail.com>
2016-04-30 23:35:22 +0500

    THIS IS AJAAAAAX!

---
+++ WebUI/Scripts/cart.js
@@ -0,0 +1,40 @@
+﻿function vote(id, returnUrl) {
+    fetch("/Cart/AddToCart", {
+            method: "post",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: "id=" + id + "&returnUrl=" + returnUrl,
+            credentials: "same-origin"
+        })
+        .then(function (data) {
+            fetch("/Cart/Cart", {
+                    credentials: 'same-origin'
+                })
+                .then(function (partialViewResult) {
+                    return partialViewResult.text();
+                })
+                .then(function(text) {
+                    document.querySelector("#cart-root").innerHTML = text;
+                });
+        });
+}
+
+function unvote(id, returnUrl) {
+    //RemoveFromCart
+    fetch("/Cart/RemoveFromCart", {
+        method: "post",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: "id=" + id + "&returnUrl=" + returnUrl,
+        credentials: "same-origin"
+    })
+        .then(function (data) {
+            fetch("/Cart/Cart", {
+                credentials: 'same-origin'
+            })
+                .then(function (partialViewResult) {
+                    return partialViewResult.text();
+                })
+                .then(function (text) {
+                    document.querySelector("#cart-root").innerHTML = text;
+                });
+        });
+}
